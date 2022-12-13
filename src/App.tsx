4356f99fc33e82f3480b3b497e326e3a78b63a5c
paulolima1212 <plima12121984@gmail.com>
2022-12-13 12:14:03 +0000

    initial: first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div>
+      <h1>Bolacha produção</h1>
+    </div>
+  );
+}
---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <div>
+      <h1>Tela encomendas</h1>
+    </div>
+  );
+}
